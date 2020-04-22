4047edb7449fcf34222691f1c26c66990e6fee93
Maciej Książkiewicz <ksiazkiewiczmaciej@outlook.com>
2020-04-22 17:50:50 +0200

    lamdas! v3

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -10,9 +10,7 @@ namespace LanguageFeatures.Controllers
     public class HomeController : Controller
     {
 
-        public ViewResult Index()
-        {
-            return View(Product.GetProducts().Select(p => p?.Name));
-        }
+        public ViewResult Index() => View(Product.GetProducts().Select(p => p?.Name));
+        
     }
 }
