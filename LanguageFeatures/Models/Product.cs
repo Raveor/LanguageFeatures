62ba0d0927659ed4840bf7ceb8b23148dc9c2ba1
Maciej Książkiewicz <ksiazkiewiczmaciej@outlook.com>
2020-04-22 16:33:17 +0200

    first mvc

---
+++ LanguageFeatures/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+
+        public static Product[] GetProducts()
+        {
+            Product kayak = new Product()
+            {
+                Name = "Kajak",
+                Price = 275M
+            };
+
+            Product lifejacket = new Product
+            {
+                Name = "Kamizelka ratunkowa",
+                Price = 48.95M
+            };
+
+            return new Product[] { kayak, lifejacket, null };
+        }
+    }
+}
--- LanguageFeatures/Startup.cs
+++ LanguageFeatures/Startup.cs
@@ -16,6 +16,7 @@ namespace LanguageFeatures
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,16 +26,17 @@ namespace LanguageFeatures
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseMvcWithDefaultRoute();
 
-            app.UseRouting();
+            //app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            /*app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
-            });
+            });*/
         }
     }
 }
