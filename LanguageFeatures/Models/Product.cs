6441ddbb9406d47a8f943b5862ec4708ecf5d8c1
Maciej Książkiewicz <ksiazkiewiczmaciej@outlook.com>
2020-04-22 17:03:42 +0200

    auto getter/setter

--- LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/Models/Product.cs
@@ -8,14 +8,22 @@ namespace LanguageFeatures.Models
     public class Product
     {
         public string Name { get; set; }
+        public string Category { get; set; } = "Wodniarstwo";
         public decimal? Price { get; set; }
         public Product Related { get; set; }
+        public bool InStock { get; }
+
+        public Product(bool inStock = true)
+        {
+            InStock = inStock;
+        }
 
         public static Product[] GetProducts()
         {
             Product kayak = new Product()
             {
                 Name = "Kajak",
+                Category = "Sporty wodne",
                 Price = 275M
             };
 
