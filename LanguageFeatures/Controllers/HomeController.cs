c445c1c54f776cdfde70ae8c2b804c5046f9bd76
Maciej Książkiewicz <ksiazkiewiczmaciej@outlook.com>
2020-04-22 17:21:48 +0200

    class extensions

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -11,21 +11,9 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            object[] data = new object[] { 275M, 29.95M, "jabłka", "pomarańcze", 100, 10 };
-            decimal total = 0;
-            for (int i = 0; i < data.Length; i++)
-            {
-                switch(data[i])
-                {
-                    case decimal decimalValue:
-                        total += decimalValue;
-                        break;
-                    case int intValue when intValue > 50:
-                        total += intValue;
-                        break;
-                }
-            }
-            return View("Index", new string[] { $"Razem: {total:C2}" });
+            ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+            decimal cartTotal = cart.TotalPrices();
+            return View("Index", new string[] { $"Razem: {cartTotal:C2}" });
         }
     }
 }
