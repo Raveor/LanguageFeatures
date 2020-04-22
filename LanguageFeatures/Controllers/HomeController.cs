ec015e6d9d8b2a0360c1ac2eb0bc83efafb957fc
Maciej Książkiewicz <ksiazkiewiczmaciej@outlook.com>
2020-04-22 17:55:53 +0200

    lambdas in properties

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -9,8 +9,6 @@ namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-
         public ViewResult Index() => View(Product.GetProducts().Select(p => p?.Name));
-        
     }
 }
--- LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/Models/Product.cs
@@ -12,6 +12,7 @@ namespace LanguageFeatures.Models
         public decimal? Price { get; set; }
         public Product Related { get; set; }
         public bool InStock { get; }
+        public bool NameBeginsWithS => Name?[0] == 'P';
 
         public Product(bool inStock = true)
         {
