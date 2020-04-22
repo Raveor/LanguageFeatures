c82132d2d8898b5feaa6f94a0554601e999a9dd0
Maciej Książkiewicz <ksiazkiewiczmaciej@outlook.com>
2020-04-22 18:15:17 +0200

    async & await

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -9,16 +9,18 @@ namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
+        public async Task<ViewResult> Index()
         {
-            var products = new[]
+            /*var products = new[]
             {
                 new { Name = "Kajak", Price = 275M },
                 new { Name = "Kamizelka ratunkowa", Price = 48.95M },
                 new { Name = "Piłka nożna", Price = 19.50M },
                 new { Name = "Flaga narożna", Price = 34.95M },
             };
-            return View(products.Select(p => p.GetType().Name));
+            return View(products.Select(p => p.GetType().Name));*/
+            long? length = await MyAsyncMethods.GetPageLength();
+            return View(new string[] { $"Długość: {length}" });
         }
     }
 }
--- LanguageFeatures/Models/MyAsyncMethods.cs
+++ LanguageFeatures/Models/MyAsyncMethods.cs
@@ -8,14 +8,11 @@ namespace LanguageFeatures.Models
 {
     public class MyAsyncMethods
     {
-        public static Task<long?> GetPageLength()
+        public async static Task<long?> GetPageLength()
         {
             HttpClient client = new HttpClient();
-            var httpTask = client.GetAsync("http://apress.com");
-            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) =>
-            {
-                return antecedent.Result.Content.Headers.ContentLength;
-            });
+            var httpTask = await client.GetAsync("http://apress.com");
+            return httpTask.Content.Headers.ContentLength;
         }
     }
 }
